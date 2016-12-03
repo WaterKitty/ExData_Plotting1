44f84fcbc32db40747d7295413152a93c71d16e0
WaterKitty <mvelagapudi1@gmail.com>
2016-12-02 20:54:28 -0600

    Code for Plot 1

---
+++ plot1.R
@@ -0,0 +1,20 @@
+## Manually download the zip file and store it as "household_power_consumption.zip". 
+## Unzip the Household Power Consumption file:
+
+unzip(zipfile="./data/household_power_consumption.zip",exdir="./data")
+
+## Read the file and store into a new variable h_table:
+
+h_data <- read.table("./data/household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+
+## Clear irrelevant data, Convert V3 into numeric, and remove NAs:
+
+library(dplyr)
+h_data <- h_data %>% 
+	select(V3) %>% 
+	mutate(V3 = as.numeric(as.character(V3)), rm.na=TRUE)
+
+## Plot the histogram:
+
+hist(h_data$V3, col = "red", main = "Global Active Power",
+     xlab = "Global Active Power (killowatts)", ylab = "Frequency")
